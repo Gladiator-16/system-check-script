8b4bfdf98c7c6d90733e79525ae177c083dd1034
Gladiator16 <64548701+Gladiator-16@users.noreply.github.com>
2021-05-30 13:14:41 +0530

    Create sys.sh

---
+++ sys.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+##
+# BASH menu script that checks:
+# - Memory usage
+# - CPU load
+# - Number of TCP connections
+# - Kernel version
+# - saves the output
+##
+server_name=$(hostname)
+function memory_check() {
+ echo ""
+ echo "Memory usage on ${server_name} is: "
+ free -h
+ echo ""
+}
+function cpu_check() {
+ echo ""
+ echo "CPU load on ${server_name} is: "
+ echo ""
+ uptime
+ echo ""
+}
+function tcp_check() {
+ echo ""
+ echo "TCP connections on ${server_name}: "
+ echo ""
+ cat /proc/net/tcp | wc -l
+ echo ""
+}
+function kernel_check() {
+ echo ""
+ echo "Kernel version on ${server_name} is: "
+ echo ""
+ uname -r
+ echo ""
+}
+function all_checks() {
+ memory_check
+ cpu_check
+ tcp_check
+ kernel_check
+}
+function output() {
+ memory_check && cpu_check && tcp_check && kernel_check >> output.txt
+}
+##
+# Color Variables
+##
+green='\e[32m'
+blue='\e[34m'
+clear='\e[0m'
+##
+# Color Functions
+##
+ColorGreen(){
+ echo -ne $green$1$clear
+}
+ColorBlue(){
+ echo -ne $blue$1$clear
+}
+menu(){
+echo -ne "
+My First Menu
+$(ColorGreen '1)') Memory usage
+$(ColorGreen '2)') CPU load
+$(ColorGreen '3)') Number of TCP connections
+$(ColorGreen '4)') Kernel version
+$(ColorGreen '5)') Check All
+$(ColorGreen '6)') Output
+$(ColorGreen '0)') Exit
+$(ColorBlue 'Choose an option:') "
+ read a
+ case $a in
+ 1) memory_check ; menu ;;
+ 2) cpu_check ; menu ;;
+ 3) tcp_check ; menu ;;
+ 4) kernel_check ; menu ;;u
+ 5) all_checks ; menu ;;
+ 6) Output ; menu ;;
+ 0) exit 0 ;;
+ *) echo -e $red"Wrong option."$clear;
+WrongCommand;;
+ esac
+}
+# Call the menu function
+menu
